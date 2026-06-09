61786307083431eaf8478bc7cc136d65bed7a2e2
Izadep <izzhadif79@gmail.com>
2026-06-09 22:52:20 +0800

    Updated MediLocator features

---
+++ HomeScreen.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Screen</title>
+    <link rel= "stylesheet" href="HomeScreen.css">
+</head>
+<body>
+    <img id="logo" src="image/MedilocatorIslam.svg">
+    <div class="search-container">
+        <a href="Search">
+            <span class="search-icon"><img src="image/magnifyingGlass.png"></span>
+        </a>
+        <input type="text" placeholder="Search Clinic or Pharmacy..." id="SearchBar">
+    </div>
+    <div id="ProfPart">
+        <a href="Profile.php">
+            <img id="ProfilePic" src="image/ProfileEmpty.png">
+        </a>
+    </div>
+    <div class="sidebar">
+        <a href="HomeScreen.php">Home</a>
+        <a href="profile.php">Profile</a>
+    </div>
+</body>
+</html>
