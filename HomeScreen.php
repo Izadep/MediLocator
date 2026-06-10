46443cae38fe2f609ff71533781eacf86ec9ba8f
Izadep <izzhadif79@gmail.com>
2026-06-10 15:05:01 +0800

    Updated MediLocator features

--- HomeScreen.php
+++ HomeScreen.php
@@ -7,21 +7,67 @@
     <link rel= "stylesheet" href="HomeScreen.css">
 </head>
 <body>
-    <img id="logo" src="image/MedilocatorIslam.svg">
+    <div class="NavBar">
+        <a href="HomeScreen.php" class="nav-item">Home</a>
+        <a href="Profile.php" class="nav-item">Profile</a>
+        <a href="Appointment.php" class="nav-item">Appointment</a>
+        <a href="Chat.php" class="nav-item">Chat</a>
+    </div>
     <div class="search-container">
         <a href="Search">
             <span class="search-icon"><img src="image/magnifyingGlass.png"></span>
         </a>
         <input type="text" placeholder="Search Clinic or Pharmacy..." id="SearchBar">
     </div>
-    <div id="ProfPart">
-        <a href="Profile.php">
-            <img id="ProfilePic" src="image/ProfileEmpty.png">
-        </a>
-    </div>
-    <div class="sidebar">
+    <!--<div class ="profile-container">
+        <div id="ProfPart">
+            <a href="Profile.php">
+                <img id="ProfilePic" src="image/ProfileEmpty.png">
+            </a>
+            <div id="Username">
+                <a>[Name]</a>
+            </div> 
+        </div>
+    </div> -->
+    <!-- <div class="sidebar">
         <a href="HomeScreen.php">Home</a>
-        <a href="profile.php">Profile</a>
+        <a href="Profile.php">Profile</a>
+        <a href="Appointment.php">Appointment</a>
+        <a href="Chat.php">Chat</a>
+    </div> -->
+    <div class="content">
+
+    <div class="mapAyerKeroh">
+        <div id="map-container">
+            <div id="loc-container">
+                <img src="image/marker-container.png" id="marker-image">
+                <div class="current-location">Melaka, Malaysia</div>
+                <div class="area-name">(Ayer Keroh)</div>
+            </div>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3986.6844052725155!2d102.28686109116023!3d2.271083557748551!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sgl!2smy!4v1781063671548!5m2!1sen!2sus"
+                id="map" allowfullscreen loading="lazy"></iframe>
+        </div>
+    </div>
+
+        <div class="quickcategories-container">
+            <div class="categories-box">
+                <div class="category">
+                    <img src="image/Clinic-icon.png" alt="">
+                    <span>Clinic</span>
+                </div>
+
+                <div class="category">
+                    <img src="image/pharmacy-icon.png" alt="">
+                    <span>Pharmacy</span>
+                </div>
+
+                <div class="category">
+                    <img src="image/specialist-icon.png" alt="">
+                    <span>Specialist</span>
+                </div>
+            </div>
+        </div>
     </div>
 </body>
 </html>
