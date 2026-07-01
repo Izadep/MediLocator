29dd5f2e1cd070b5ae8daf29bf72536fc285874c
asyhdina <aisyahaddinaa@gmail.com>
2026-07-01 22:03:28 +0800

    Updated branch

--- database.php
+++ database.php
@@ -3,8 +3,9 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "medilocator";
+$root = "3307";
 
-$conn = mysqli_connect($servername,$username,$password,$dbname);
+$conn = mysqli_connect($servername,$username,$password,$dbname, $root, 8080);
 
 if(!$conn) {
     die("Database connection failed: ". mysqli_connect_errno());
